1ac99efea22ad0019135e5a215d02f928788be4b
priyesh3016 <44001421+priyesh3016@users.noreply.github.com>
2023-11-18 10:16:00 +0000

    javascript basics

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = true;
+const valueInNumber = 11
+//console.log(typeof(score));
+//console.log(typeof score);
+
+
+//let valueInNumber = Number(score)
+console.log(valueInNumber);
---
+++ 01_Basics/Test.js
@@ -0,0 +1,3 @@
+console.log("test");
+    
+console.log(1 + 2);
